5b3347e32808fb49ae76ae96a204231a210c8894
Scoob <thatguyscoob1@gmail.com>
2024-04-23 23:00:12 +0000

    Added decryption option, as well as start and end randomized letters

--- main.py
+++ main.py
@@ -1,17 +1,48 @@
+import random
 import string
 
 alphabet = string.ascii_lowercase
 
-userString = input('Input String:')
-shift = int(input('Input Shift:'))
-result = ''
+state = input('Do you want to encrypt or decrypt? [E/D]: ')
+userString = input('Input String: ')
+shift = int(input('Input Shift: '))
+start = int(input('How many random letters on the start: '))
+end = int(input('How many random letters on the end: '))
 
-for char in userString:
-  if char.isalpha():
-    shiftedIndex = (alphabet.index(char.lower()) + shift) % 26
-    shiftedChar = alphabet[shiftedIndex]
-    result += shiftedChar
-  else:
-    result += char
+def encrypt(userString, shift):
+  result = ''
+  tempResult = ''
+  randEnd = ''
+  randStart = ''
+  for char in userString:
+    if char.isalpha():
+      shiftedIndex = (alphabet.index(char.lower()) + shift) % 26
+      shiftedChar = alphabet[shiftedIndex]
+      randStart = ''.join(random.choice(alphabet) for _ in range(start))
+      randEnd = ''.join(random.choice(alphabet) for _ in range(end))
+      tempResult += shiftedChar
+    else:
+      tempResult += char
 
-print(f'Encrypted String: {result}')
+  result = randStart + tempResult + randEnd
+  print(f'Encrypted String: {result}')
+
+def decrypt(userString, shift, start, end):
+  result = ''
+  tempResult = ''
+  for char in userString:
+    if char.isalpha():
+      shiftedIndex = (alphabet.index(char.lower()) - shift) % 26
+      shiftedChar = alphabet[shiftedIndex]
+      tempResult += shiftedChar
+    else:
+      tempResult += char
+  result = tempResult[start:-end]
+  print(f'Decrypted String: {result}')
+
+if state == 'E':
+  encrypt(userString, shift,)
+elif state == 'D':
+  decrypt(userString, shift, start, end)
+else:
+  print('Invalid input. Try again.')
